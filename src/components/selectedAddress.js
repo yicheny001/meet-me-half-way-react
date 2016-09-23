9ea4e698f36dc54eb5238b705b20119f48bdea59
leearaneta <lee.araneta@gmail.com>
2016-09-22 22:23:38 -0400

    nbsp

--- src/components/selectedAddress.js
+++ src/components/selectedAddress.js
@@ -3,7 +3,7 @@ import React from 'react'
 const SelectedAddress = ({address, remove}) => {
   return (
     <div className='capCalibri'>
-      <label>{address.name}</label>
+      {address.name} &nbsp;
       <button onClick={remove} type="submit" className="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab mdl-button--colored"><i className="material-icons">clear</i></button>
     </div>
   )
