cb26af348a9509430d7b1c0a656d8143076c7417
leearaneta <lee.araneta@gmail.com>
2016-10-04 23:42:42 -0400

    minor styling

--- src/components/filters.js
+++ src/components/filters.js
@@ -3,29 +3,52 @@ import Toggle from 'material-ui/Toggle'
 import TextField from 'material-ui/TextField'
 import Slider from 'material-ui/Slider'
 
-const styles = {
-  toggle: {
-    width: 120
-  },
-  textField: {
-    width: 140
-  }
+const textStyle = {
+  display: 'inline-block',
+  marginRight: 5,
+  marginLeft: 5,
+  width: 135
 }
+
+const toggleStyle = {...textStyle, paddingLeft: 10, paddingRight: 10}
+
 
 const Filters = ({handleChange, handleToggle, handleDragRadius, handleStopRadius, handleDragPrice, handleStopPrice}) => {
   return (
     <div className='filters'>
-      <Toggle label="Open Now" style={styles.toggle} onToggle={handleToggle} />
+      <Toggle label="Open Now"
+      style={toggleStyle}
+      onToggle={handleToggle}
+      />
       <TextField
       type="number"
-      style={styles.textField}
+      style={textStyle}
       floatingLabelText="How many results?"
       onChange={handleChange}
       defaultValue={3}
       min={1}
       />
-      <Slider defaultValue={1} min={0.5} max={1.5} onChange={handleDragRadius} onDragStop={handleStopRadius}/>
-      <Slider defaultValue={4} min={1} max={4} step={1} onChange={handleDragPrice} onDragStop={handleStopPrice}/>
+      <div className='slider-container'>
+        <div className='slider-label'>Maximum Distance</div>
+        <Slider
+        defaultValue={1}
+        min={0.5}
+        max={1.5}
+        onChange={handleDragRadius}
+        onDragStop={handleStopRadius}
+        />
+      </div>
+      <div className='slider-container'>
+        <div className='slider-label'>Maximum Price</div>
+        <Slider
+        defaultValue={4}
+        min={1}
+        max={4}
+        step={1}
+        onChange={handleDragPrice}
+        onDragStop={handleStopPrice}
+        />
+      </div>
     </div>
   )
 }
