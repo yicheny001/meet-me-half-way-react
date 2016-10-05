16231e1ee7e582f740bc1eeb0e5910f090ad8b68
yyangflatiron <yichen.yang@flatironschool.com>
2016-10-05 13:39:53 -0400

    fixes

--- src/components/filters.js
+++ src/components/filters.js
@@ -2,30 +2,57 @@ import React from 'react'
 import Toggle from 'material-ui/Toggle'
 import TextField from 'material-ui/TextField'
 import Slider from 'material-ui/Slider'
+import { yellow600, yellow200, blue200, blue500 } from 'material-ui/styles/colors'
 
 const styles = {
   toggle: {
-    width: 120
+    width: 120,
+    didFlip: true,
+    position: 'relative',
+    cursor: 'pointer',
+    overflow: 'visible',
+    fill: blue500,
+    color: blue500,
   },
-  textField: {
-    width: 140
+  underline:{
+    width: '65%',
+  },
+  textField:{
+    color: blue500,
+    borderColor: blue500,
+    width: 150,
+  },
+
+  slider:{
+    width:100,
+    fill: blue500,
+    border: blue500,
   }
 }
 
 const Filters = ({handleChange, handleToggle, handleDragRadius, handleStopRadius, handleDragPrice, handleStopPrice}) => {
   return (
-    <div className='filters'>
-      <Toggle label="Open Now" style={styles.toggle} onToggle={handleToggle} />
+    <div>
+      <br />
+      <Toggle
+        label="OPEN NOW"
+        elementStyle={styles.toggle}
+        style={styles.toggle}
+        onToggle={handleToggle}
+      />
       <TextField
       type="number"
       style={styles.textField}
-      floatingLabelText="How many results?"
+      floatingLabelText="HOW MANY RESULTS?"
       onChange={handleChange}
       defaultValue={3}
       min={1}
+      underlineStyle={styles.textField}
+      underlineFocusStyle={styles.textField}
+      floatingLabelFocusStyle={styles.textField}
       />
-      <Slider defaultValue={1} min={0.5} max={1.5} onChange={handleDragRadius} onDragStop={handleStopRadius}/>
-      <Slider defaultValue={4} min={1} max={4} step={1} onChange={handleDragPrice} onDragStop={handleStopPrice}/>
+      <Slider style={styles.slider} defaultValue={1} min={0.5} max={1.5} onChange={handleDragRadius} onDragStop={handleStopRadius}/>
+      <Slider sliderStyle={styles.slider} defaultValue={4} min={1} max={4} step={1} onChange={handleDragPrice} onDragStop={handleStopPrice}/>
     </div>
   )
 }
--- src/components/queryForm.js
+++ src/components/queryForm.js
@@ -7,13 +7,26 @@ const styles = {
   label: {
     color:'#fff',
     letterSpacing: '0.1em'
+  },
+  textField:{
+    color: blue500,
+    borderColor: blue500
   }
 }
 
 const QueryForm = ({onSubmit}) => {
   return (
     <form className='query-form' onSubmit={onSubmit}>
-      <TextField className='query-text' hintText="Try 'Pizza' or 'Starbucks'!" floatingLabelText="WHAT ARE YOU IN THE MOOD FOR?" />
+      <TextField
+        underlineStyle={styles.textField}
+        underlineFocusStyle={styles.textField}
+        floatingLabelFocusStyle={styles.textField}
+
+        className='query-text'
+        hintText="Try 'Pizza' or 'Starbucks'!"
+        floatingLabelText="WHAT ARE YOU IN THE MOOD FOR?"
+      />
+      <br/>
       <br/>
       <FlatButton type='submit' label="FIND PLACES!" backgroundColor={blue500} labelStyle={styles.label}	hoverColor={yellow200} /> &nbsp; &nbsp; &nbsp;
       <FlatButton label="CLEAR" backgroundColor={yellow600} labelStyle={styles.label}	hoverColor={blue200} />
